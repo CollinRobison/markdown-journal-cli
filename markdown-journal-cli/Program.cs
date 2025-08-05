ac5e2fd052448562e5c9b6d4ef6bd99e908fffd5
Collin Robison <collinrobison@gmail.com>
2025-08-05 16:58:48 -0500

    added barebone with code from spectre example

---
+++ markdown-journal-cli/Program.cs
@@ -0,0 +1,35 @@
+﻿using Demo.Commands.Add;
+using Demo.Commands.Run;
+using Demo.Commands.Serve;
+using Spectre.Console.Cli;
+
+namespace markdown_journal_cli;
+
+public static class Program
+{
+    public static int Main(string[] args)
+    {
+        var app = new CommandApp();
+        app.Configure(config =>
+        {
+            config.SetApplicationName("md-journal");
+            config.ValidateExamples();
+            config.AddExample("run", "--no-build");
+
+            // Run
+            config.AddCommand<RunCommand>("run");
+
+            // Add
+            // config.AddBranch<AddSettings>("add", add =>
+            // {
+            //     add.SetDescription("Add a package or reference to a .NET project");
+            //     add.AddCommand<AddPackageCommand>("package");
+            //     add.AddCommand<AddReferenceCommand>("reference");
+            // });
+
+            
+        });
+
+        return app.Run(args);
+    }
+}
